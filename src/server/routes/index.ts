d23dcd65bae83da57ec0d1a7fc36d19b4512d3f6
Lazaro-Costa <lazarocosta.dev@gmail.com>
2023-10-09 08:55:22 -0300

    Feat: Validação de rotas implementadas

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -6,5 +6,24 @@ const router = Router();
 router.get('/', (_, res)=>{
   res.send('Home Site');
 });
-router.post('/cidades', CidadesController.createBodyValidator ,CidadesController.create);
+router.get('/cidades', 
+  CidadesController.getAllValidation,
+  CidadesController.getAll);
+
+router.get('/cidades/:id', 
+  CidadesController.getByIdValidation,
+  CidadesController.getById);
+
+router.put('/cidades/:id', 
+  CidadesController.updateByIdValidation,
+  CidadesController.updateById);
+
+router.delete('/cidades/:id', 
+  CidadesController.deleteByIdValidation,
+  CidadesController.deleteById);
+
+router.post('/cidades', 
+  CidadesController.createValidation,
+  CidadesController.create);
+
 export { router };
